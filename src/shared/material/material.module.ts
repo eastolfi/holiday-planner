f4f80f93c766ba7a3a6465cc670946edb2dc7408
Eduardo Astolfi <eastolfi91@gmail.com>
2019-12-19 09:49:21 +0100

    chore: Add a shared material module and some deps

--- src/shared/material/material.module.ts
+++ src/shared/material/material.module.ts
@@ -1,41 +1,37 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCardModule } from '@angular/material/card';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 const matModules = [
-  MatButtonModule,
-  MatCardModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatToolbarModule,
-  MatSidenavModule,
-  MatRadioModule,
-  MatSelectModule,
-  MatGridListModule,
-  MatMenuModule,
-  MatTableModule,
-  MatPaginatorModule,
-  MatTooltipModule
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatGridListModule,
+    MatCardModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatCheckboxModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
 ];
 
 @NgModule({
-  imports: matModules,
-  exports: matModules,
+    imports: matModules,
+    exports: matModules,
 })
 export class MaterialModule {
 }
